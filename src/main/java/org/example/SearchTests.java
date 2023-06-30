6f5331b55c83c42b21db59d12996fe1098d26b59
Jonatan <jonathan.leon.caparros@gmail.com>
2023-06-30 18:36:53 +0200

    added steps and expected results to the tests

--- src/main/java/org/example/SearchTests.java
+++ src/main/java/org/example/SearchTests.java
@@ -26,76 +26,77 @@ public class SearchTests {
         searchBar = driver.findElement(By.id("q"));
     }
 
-    /**
-     * Checks that the results of a search match what is being searched.
-     */
     @Test
     public void validSearchQueryTest() {
         search = "gandalf";
 
+        // 1. User makes a valid search
         enterSearch(search);
+        // 2. Validates that the results page is displayed
         Assert.assertTrue(driver.findElement(By.className("search-info"))
                 .isDisplayed());
+        // 3. Validates the results of the search
+        // Expected result: the list of results matches the user's query
         assertResults(search);
     }
 
-    /**
-     * Checks that the page can handle when there are no results for an invalid search.
-     */
     @Test
     public void invalidSearchQueryTest() {
         search = "~~@#~@#~@!!!";
-
+        // 1. User makes an invalid search
         enterSearch(search);
+        // 2. Validates the results of the search
+        // Expected result: there are no results for an invalid search
         Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"main\"]/div[3]/div/h1"))
                 .getText(), "No cards found");
         assertNoResults();
     }
 
-    /**
-     * Checks that the page can handle when the search is empty.
-     */
     @Test
     public void emptySearchQueryTest() {
+        // 1. User makes an empty search
         searchBar.sendKeys(Keys.ENTER);
+        // 2. Validates the results of the search
+        // Expected result: there are no results for an empty search.
+        // A specific error message for that is displayed
         Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/p"))
                 .getText(), "You didn‘t enter anything to search for.");
         assertNoResults();
     }
 
-    /**
-     * Checks that the results can be sorted according to the user's preferences,
-     * for example by release date.
-     */
     @Test
     public void searchResultSortingTest() {
         String sortByDate = "//*[@id=\"order\"]/option[2]";
 
+        // 1. User makes a valid search
         enterSearch("lotus");
+        // 2. The type of sorting is selected after the results have appeared
         List<WebElement> dropdownMenu = driver.findElements(By.id("order"));
         dropdownMenu.get(1).click();
         driver.findElement(By.xpath(sortByDate)).click();
+        // 3. Validates the sorting
+        // Expected result: the chosen sorting by release date is applied
         Assert.assertEquals(driver.findElement(By.xpath(sortByDate))
                 .getAttribute("selected"), "true");
     }
 
-    /**
-     * Checks that the results are correctly paginated when there are too many
-     */
     @Test
     public void paginationTest() {
         search = "mage";
         String next60ResultsButton = "//*[@id=\"main\"]/div[1]/div/div[2]/a[1]";
 
+        // 1. User makes a valid search that returns too many elements
         enterSearch(search);
-        // First it asserts the results of the first page and if pagination is enabled
+        // 2. First it validates the results of the first page and if pagination is enabled
         assertResults(search);
         Assert.assertTrue(driver.findElement(By.xpath(next60ResultsButton))
                 .isEnabled());
-        // Then it goes to the second page and asserts
+        // 3. Then it goes to the second page and validates the results
         driver.findElement(By.xpath(next60ResultsButton)).click();
         assertResults(search);
-        // Finally it goes to the third page and asserts again
+        // 4. Finally, it goes to the third page and validates the results again
+        // Expected result: pagination controls are enabled
+        // and every page matches the results of the search
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/div/div[2]/a[3]"))
                 .click();
         assertResults(search);
